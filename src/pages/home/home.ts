040976857ab13b60f6514c93385106926de0cf52
Dame <camaradame@gmail.com>
2018-09-04 20:19:00 +0000

    new oush

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Pro} from "@ionic/pro";
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,23 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    this.checkForUpdate();
+  }
+  async checkForUpdate() {
+    const update = await Pro.deploy.checkForUpdate()
+    if (update.available){
+      // We have an update!
+
+      console.log("checkForUpdate ===> Mis a jour dispo")
+      await Pro.deploy.downloadUpdate((progress) => {
+        console.log("Progression"+JSON.stringify(progress));
+      })
+      await Pro.deploy.extractUpdate((progress) => {
+        console.log(progress);
+      })
+      await Pro.deploy.reloadApp();
+    }
+    else  console.log("checkForUpdate ===> Pas de Mis a jour dispo")
 
   }
-
 }
